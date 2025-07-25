c1f267f14eee72a029c4684c9483c9bfa2ea8778
lubswer <lubswer@hotmail.com>
2025-07-24 22:00:50 -0500

    Se avanzo con el login

--- build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/cmake_install.cmake
+++ build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Stalin/Documents/Gestion_Atletas_Final
+# Install script for directory: C:/Users/usuario/Gestion_Atletas_Final
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -38,7 +38,7 @@ if(NOT DEFINED CMAKE_OBJDUMP)
 endif()
 
 if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "C:/Users/Stalin/Documents/Gestion_Atletas_Final/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/Gestion_Atletas_Final.exe")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "C:/Users/usuario/Gestion_Atletas_Final/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/Gestion_Atletas_Final.exe")
   if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/Gestion_Atletas_Final.exe" AND
      NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/Gestion_Atletas_Final.exe")
     if(CMAKE_INSTALL_DO_STRIP)
@@ -62,6 +62,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "C:/Users/Stalin/Documents/Gestion_Atletas_Final/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/usuario/Gestion_Atletas_Final/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include "mainwindow.h"
+#include "logindialog.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    LoginDialog loginDialog;
+    if (loginDialog.exec()== QDialog::Accepted){
+        MainWindow w;
+        w.show();
+
+        return a.exec();
+    }
+
+    return 0;
 }
